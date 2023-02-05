eea6feec072788f10eafd784d5a0f5c4ac0f0c78
is121018 <christoph@syslifters.com>
2023-02-05 09:18:54 -0800

    00fa7b4a6c7f007d45d95e6775b0f628934bd480

---
+++ pingcastle/Scanners/localAdminsScanner.cs
@@ -0,0 +1,45 @@
+﻿//
+// Copyright (c) Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+
+namespace PingCastle.Scanners
+{
+    public class localAdminsScanner : ScannerBase
+    {
+
+        public override string Name { get { return "localadmin"; } }
+        public override string Description { get { return "Enumerate the local administrators of a computer."; } }
+
+        override protected string GetCsvHeader()
+        {
+            return "Computer\tSID\tAccount";
+        }
+
+        override protected string GetCsvData(string computer)
+        {
+            string output = null;
+            List<SecurityIdentifier> users = localAdminsEnumerator.Export(computer);
+
+            foreach (SecurityIdentifier user in users)
+            {
+                string account = ConvertSIDToName(user.Value, computer);
+                if (!String.IsNullOrEmpty(output))
+                    output += "\r\n";
+                output += computer + "\t" + user.Value + "\t" + account;
+            }
+            return output;
+        }
+
+        static string ConvertSIDToName(string sidstring, string server)
+        {
+            string referencedDomain = null;
+            return NativeMethods.ConvertSIDToNameWithWindowsAPI(sidstring, server, out referencedDomain);
+        }
+    }
+}
