e3ae69e9524d51f5b46c2faab49589a4504adec6
is121018 <christoph@syslifters.com>
2023-02-05 08:16:47 -0800

    6ae45bf211957195590cc04ee24ceaacb9131d49

---
+++ pingcastle/PingCastleException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace PingCastle
+{
+    [Serializable]
+    public class PingCastleException : Exception
+    {
+        public PingCastleException()
+        {
+        }
+
+        public PingCastleException(string message)
+            : base(message)
+        {
+        }
+
+        public PingCastleException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected PingCastleException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
