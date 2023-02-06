eb0eab0df7c476119b4c4cfdb9b000c752bad191
cmahrl <christoph@syslifters.com>
2023-02-06 08:12:11 -0800

    vletoux/pingcastle@3ffdf47a857c9c47cf3105ab83b4442ef73ca4ce

---
+++ pingcastle/Healthcheck/Rules/HeatlcheckRuleStaledJavaSchema.cs
@@ -0,0 +1,27 @@
+﻿//
+// Copyright (c) Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+using PingCastle.Rules;
+
+namespace PingCastle.Healthcheck.Rules
+{
+    [RuleModel("S-JavaSchema", RiskRuleCategory.StaleObjects, RiskModelCategory.ObjectConfig)]
+    [RuleComputation(RuleComputationType.TriggerOnPresence, 0)]
+    [RuleMitreAttackMitigation(MitreAttackMitigation.ActiveDirectoryConfiguration)]
+    [RuleMaturityLevel(3)]
+    [RuleIntroducedIn(2,10,1)]
+    public class HeatlcheckRuleStaledJavaSchema : RuleBase<HealthcheckData>
+    {
+        protected override int? AnalyzeDataNew(HealthcheckData healthcheckData)
+        {
+            if (healthcheckData.JavaClassFound)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
