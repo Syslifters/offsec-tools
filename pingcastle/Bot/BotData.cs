96e782674598ba93bf7964fb485920aec54b1878
cmahrl <christoph@syslifters.com>
2023-02-06 06:19:30 -0800

    vletoux/pingcastle@3ffdf47a857c9c47cf3105ab83b4442ef73ca4ce

---
+++ pingcastle/Bot/BotData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace PingCastle.Bot
+{
+    public class BotInputOutput
+    {
+        public List<BotData> Data { get; set; }
+
+    }
+    public class BotData
+    {
+        public BotData()
+        {
+
+        }
+        public BotData(string Key, string Value) : this()
+        {
+            this.Key = Key;
+            this.Value = Value;
+        }
+
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
