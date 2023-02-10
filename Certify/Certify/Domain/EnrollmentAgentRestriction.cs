fcda872b0b6b699a35e2622730819353050c489f
cmahrl <christoph@syslifters.com>
2023-02-10 11:05:38 -0800

    GhostPack/Certify@fb297ad30476cfdba745b9062171cd7ac145a16d

---
+++ Certify/Certify/Domain/EnrollmentAgentRestriction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Security;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using Certify.Lib;
+using Microsoft.Win32;
+using System.Text;
+
+namespace Certify.Domain
+{
+    class EnrollmentAgentRestriction
+    {
+        public string Agent { get; }
+
+        public string Template { get; }
+
+        public List<string> Targets { get; }
+
+        public EnrollmentAgentRestriction(CommonAce ace)
+        {
+            Targets = new List<string>();
+            var index = 0;
+
+            Agent = ace.SecurityIdentifier.ToString();
+            var bytes = ace.GetOpaque();
+
+            var sidCount = BitConverter.ToUInt32(bytes, index);
+            index += 4;
+
+            for (var i = 0; i < sidCount; ++i)
+            {
+                var sid = new SecurityIdentifier(bytes, index);
+                Targets.Add(sid.ToString());
+                index += sid.BinaryLength;
+            }
+
+            if (index < bytes.Length)
+            {
+                Template = Encoding.Unicode.GetString(bytes, index, (bytes.Length - index - 2)).Replace("\u0000", string.Empty);
+            }
+            else
+            {
+                Template = "<All>";
+            }
+        }
+    }
+}
