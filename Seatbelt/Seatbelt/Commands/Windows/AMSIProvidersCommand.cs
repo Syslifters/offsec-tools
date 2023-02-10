1ad8da4c2c2cf50c81a0f987e8d580d0d9534174
cmahrl <christoph@syslifters.com>
2023-02-10 11:06:08 -0800

    GhostPack/Seatbelt@071ad593bc698635e6d637c0ff69cc102ff6eebc

---
+++ Seatbelt/Seatbelt/Commands/Windows/AMSIProvidersCommand.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+
+namespace Seatbelt.Commands.Windows
+{
+    internal class AMSIProviderCommand : CommandBase
+    {
+        public override string Command => "AMSIProviders";
+        public override string Description => "Providers registered for AMSI";
+        public override CommandGroup[] Group => new[] { CommandGroup.System, CommandGroup.Remote };
+        public override bool SupportRemote => true;
+        public Runtime ThisRunTime;
+
+        public AMSIProviderCommand(Runtime runtime) : base(runtime)
+        {
+            ThisRunTime = runtime;
+        }
+
+        public override IEnumerable<CommandDTOBase?> Execute(string[] args)
+        {
+            var providers = ThisRunTime.GetSubkeyNames(RegistryHive.LocalMachine, @"SOFTWARE\Microsoft\AMSI\Providers") ?? new string[] {};
+            foreach (var provider in providers)
+            {
+                var ProviderPath = ThisRunTime.GetStringValue(RegistryHive.LocalMachine, $"SOFTWARE\\Classes\\CLSID\\{provider}\\InprocServer32", "");
+
+                yield return new AMSIProviderDTO(
+                    provider,
+                    ProviderPath
+                );
+            }
+        }
+
+        internal class AMSIProviderDTO : CommandDTOBase
+        {
+            public AMSIProviderDTO(string guid, string? providerPath)
+            {
+                GUID = guid;
+                ProviderPath = providerPath;    
+            }
+            public string GUID { get; set; }
+            public string? ProviderPath { get; set; }
+        }
+    }
+}
