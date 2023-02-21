96c1c380ce84191653b85c92a32b7a4dcd038c70
cmahrl <christoph@syslifters.com>
2023-02-21 08:01:54 -0800

    vletoux/pingcastle@8e4aaf9fff310dcee1e90b1d3797b74ffc57a34f

---
+++ pingcastle/Healthcheck/Rules/HeatlcheckRulePrivilegedSchemaAdmins.cs
@@ -0,0 +1,35 @@
+﻿using PingCastle.Graph.Reporting;
+//
+// Copyright (c) Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+using PingCastle.Rules;
+
+namespace PingCastle.Healthcheck.Rules
+{
+    [RuleModel("P-SchemaAdmin", RiskRuleCategory.PrivilegedAccounts, RiskModelCategory.IrreversibleChange)]
+    [RuleComputation(RuleComputationType.TriggerOnPresence, 10)]
+    [RuleSTIG("V-72835", "Membership to the Schema Admins group must be limited", STIGFramework.Forest)]
+    [RuleANSSI("R13", "subsection.3.2")]
+    [RuleMaturityLevel(3)]
+    [RuleMitreAttackMitigation(MitreAttackMitigation.PrivilegedAccountManagement)]
+    public class HeatlcheckRulePrivilegedSchemaAdmins : RuleBase<HealthcheckData>
+    {
+        protected override int? AnalyzeDataNew(HealthcheckData healthcheckData)
+        {
+            foreach (HealthCheckGroupData group in healthcheckData.PrivilegedGroups)
+            {
+                if (group.GroupName == GraphObjectReference.SchemaAdministrators)
+                {
+                    if (group.NumberOfMember > 0)
+                    {
+                        return group.NumberOfMember;
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
