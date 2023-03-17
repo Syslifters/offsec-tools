1b03191f527388bcf7766ab770046401535ec27c
cmahrl <christoph@syslifters.com>
2023-03-17 12:03:39 -0700

    vletoux/pingcastle@46f477547d890fbc8cd853f2fb1d15ebfd41204b

---
+++ pingcastle/Healthcheck/Rules/HeatlcheckRuleAnomalyDsHeuristicsAnonymous.cs
@@ -0,0 +1,27 @@
+﻿//
+// Copyright (c) Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+using PingCastle.Rules;
+
+namespace PingCastle.Healthcheck.Rules
+{
+    [RuleModel("A-DsHeuristicsAnonymous", RiskRuleCategory.Anomalies, RiskModelCategory.Reconnaissance)]
+    [RuleComputation(RuleComputationType.TriggerOnPresence, 5)]
+    [RuleSTIG("V-8555", "Anonymous Access to AD forest data above the rootDSE level must be disabled. ", STIGFramework.Forest)]
+    [RuleDurANSSI(2, "dsheuristics_bad", "Dangerous dsHeuristics settings")]
+    [RuleMitreAttackTechnique(MitreAttackTechnique.BruteForcePasswordSpraying)]
+    public class HeatlcheckRuleAnomalyDsHeuristicsAnonymous : RuleBase<HealthcheckData>
+    {
+        protected override int? AnalyzeDataNew(HealthcheckData healthcheckData)
+        {
+            if (healthcheckData.DSHeuristics != null && healthcheckData.DSHeuristics.Length >= 7 && healthcheckData.DSHeuristics.Substring(6, 1) == "2")
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
