7b6beb21e02809cec8b3788063b54c1b5723faa6
cmahrl <christoph@syslifters.com>
2023-03-21 05:49:37 -0700

    5d0b2220d99ba81689e20b7105a5158f4d78b2d7

--- repos/Inveigh/Inveigh.psm1
+++
@@ -1,9 +0,0 @@
-<#
-.SYNOPSIS
-Inveigh is a Windows PowerShell ADIDNS/LLMNR/mDNS/NBNS spoofer/man-in-the-middle tool.
-
-.LINK
-https://github.com/Kevin-Robertson/Inveigh
-#>
-Import-Module $PWD\Inveigh.ps1
-Import-Module $PWD\Inveigh-Relay.ps1
