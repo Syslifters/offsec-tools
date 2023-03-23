1280a60ebbf2c25685a90432a2cb1437b89ea655
cmahrl <christoph@syslifters.com>
2023-03-23 05:35:14 -0700

    carlospolop/PEASS-ng@ded6f3045ff9de93e9f60a6700432c70139e31b0

---
+++ repos/PEASS-ng/winPEAS/winPEASexe/winPEAS/Info/SystemInfo/WindowsDefender/WindowsDefenderSettingsInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace winPEAS.Info.SystemInfo.WindowsDefender
+{
+    class WindowsDefenderSettingsInfo
+    {
+        public WindowsDefenderSettings LocalSettings { get; set; }
+        public WindowsDefenderSettings GroupPolicySettings { get; set; }
+
+        public WindowsDefenderSettingsInfo(WindowsDefenderSettings localSettings, WindowsDefenderSettings groupPolicySettings)
+        {
+            LocalSettings = localSettings;
+            GroupPolicySettings = groupPolicySettings;
+        }
+    }
+}
