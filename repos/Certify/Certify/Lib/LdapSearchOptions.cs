2d6345825966a66c9787ccde4103a7643d666e11
cmahrl <christoph@syslifters.com>
2023-04-17 00:50:03 -0700

    GhostPack/Certify@fb297ad30476cfdba745b9062171cd7ac145a16d

---
+++ repos/Certify/Certify/Lib/LdapSearchOptions.cs
@@ -0,0 +1,17 @@
+﻿namespace Certify.Lib
+{
+    class LdapSearchOptions
+    {
+        public LdapSearchOptions()
+        {
+            Domain = null;
+            LdapServer = null;
+            //AuthenticationType = null;
+            //Credential = null;
+        }
+        public string? Domain { get; set; }
+        public string? LdapServer { get; set; }
+        //public AuthenticationTypes? AuthenticationType { get; set; }
+        //public NetworkCredential? Credential { get; set; }
+    }
+}
