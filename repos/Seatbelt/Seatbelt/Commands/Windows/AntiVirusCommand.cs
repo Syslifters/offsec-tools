e774d9b7d4899f62082bb075cf529b09fab06876
cmahrl <christoph@syslifters.com>
2023-04-28 11:09:49 -0700

    GhostPack/Seatbelt@96bd958cf45e3d877d842ce20906e1aa5fdc91c8

---
+++ repos/Seatbelt/Seatbelt/Commands/Windows/AntiVirusCommand.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Seatbelt.Interop;
+
+namespace Seatbelt.Commands.Windows
+{
+    internal class AntiVirusCommand : CommandBase
+    {
+        public override string Command => "AntiVirus";
+        public override string Description => "Registered antivirus (via WMI)";
+        public override CommandGroup[] Group => new[] { CommandGroup.System, CommandGroup.Remote };
+        public override bool SupportRemote => true;
+        public Runtime ThisRunTime;
+
+        public AntiVirusCommand(Runtime runtime) : base(runtime)
+        {
+            ThisRunTime = runtime;
+        }
+
+        public override IEnumerable<CommandDTOBase?> Execute(string[] args)
+        {
+            if (String.IsNullOrEmpty(ThisRunTime.ComputerName) && Shlwapi.IsWindowsServer())
+            {
+                WriteHost("Cannot enumerate antivirus. root\\SecurityCenter2 WMI namespace is not available on Windows Servers");
+                yield break;
+            }
+
+            // lists installed VA products via WMI (the AntiVirusProduct class)
+
+            var AVResults = new List<AntiVirusDTO>();
+
+            try
+            {
+                var wmiData = ThisRunTime.GetManagementObjectSearcher(@"root\SecurityCenter2", "SELECT * FROM AntiVirusProduct");
+                var data = wmiData.Get();
+
+                foreach (var virusChecker in data)
+                {
+                    AVResults.Add(new AntiVirusDTO(
+                        virusChecker["displayName"],
+                        virusChecker["pathToSignedProductExe"],
+                        virusChecker["pathToSignedReportingExe"]
+                    ));
+                }
+            }
+            catch { }
+
+            foreach(var AVResult in AVResults)
+            {
+                yield return AVResult;
+            }
+        }
+    }
+
+    internal class AntiVirusDTO : CommandDTOBase
+    {
+        public AntiVirusDTO(object engine, object productExe, object reportingExe)
+        {
+            Engine = engine;
+            ProductEXE = productExe;
+            ReportingEXE = reportingExe;
+        }
+        public object Engine { get; }
+        public object ProductEXE { get; }
+        public object ReportingEXE { get; }
+    }
+}
---
+++ repos/Seatbelt/Seatbelt/Commands/Windows/EnvironmentVariableCommand.cs
@@ -0,0 +1,82 @@
+﻿#nullable disable
+using Microsoft.Win32;
+using Seatbelt.Util;
+using System;
+using System.Collections.Generic;
+using Seatbelt.Output.TextWriters;
+using Seatbelt.Output.Formatters;
+
+
+namespace Seatbelt.Commands.Windows
+{
+    internal class EnvironmentVariableCommand : CommandBase
+    {
+        public override string Command => "EnvironmentVariables";
+        public override string Description => "Current environment variables";
+        public override CommandGroup[] Group => new[] {CommandGroup.System, CommandGroup.Remote};
+        public override bool SupportRemote => true;
+        public Runtime ThisRunTime;
+
+        public EnvironmentVariableCommand(Runtime runtime) : base(runtime)
+        {
+            ThisRunTime = runtime;
+        }
+
+        public override IEnumerable<CommandDTOBase?> Execute(string[] args)
+        {
+            var envVariables = new List<EnvironmentVariableDTO>();
+
+            try
+            {
+                var wmiData = ThisRunTime.GetManagementObjectSearcher(@"root\cimv2", "Select UserName,Name,VariableValue from win32_environment");
+                var data = wmiData.Get();
+
+                foreach (var envVariable in data)
+                {
+                    envVariables.Add(new EnvironmentVariableDTO(
+                        envVariable["UserName"],
+                        envVariable["Name"],
+                        envVariable["VariableValue"]
+                    ));
+                }
+            }
+            catch { }
+
+            foreach (var envVariable in envVariables)
+            {
+                yield return envVariable;
+            }
+
+        }
+
+        internal class EnvironmentVariableDTO : CommandDTOBase
+        {
+            public EnvironmentVariableDTO(object userName, object name, object value)
+            {
+                UserName = userName.ToString();
+                Name = name.ToString();
+                Value = value.ToString();
+            }
+            public string UserName { get; set; }
+            public string Name { get; set; }
+            public string Value { get; set; }
+        }
+
+
+        [CommandOutputType(typeof(EnvironmentVariableDTO))]
+        internal class EnvironmentVariableFormatter : TextFormatterBase
+        {
+            public EnvironmentVariableFormatter(ITextWriter writer) : base(writer)
+            {
+            }
+
+            public override void FormatResult(CommandBase? command, CommandDTOBase result, bool filterResults)
+            {
+                var dto = (EnvironmentVariableDTO)result;
+
+                WriteLine("  {0,-35}{1,-35}{2}", dto.UserName, dto.Name, dto.Value);
+            }
+        }
+    }
+}
+#nullable enable
