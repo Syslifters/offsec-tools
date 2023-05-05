07fc434b6ff4c7af457199bc528474ba9390f2a5
cmahrl <christoph@syslifters.com>
2023-05-05 11:12:52 -0700

    carlospolop/PEASS-ng@2963e478664aac057376998f0910c6f25ae63250

---
+++ repos/PEASS-ng/winPEAS/winPEASexe/winPEAS/Helpers/CustomFileInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace winPEAS.Helpers
+{
+    internal class CustomFileInfo
+    {
+        public string Filename { get; }
+        public string Extension { get; }
+        public string FullPath { get; }
+        public long Size { get; }
+        public bool IsDirectory { get; }
+
+        public CustomFileInfo(string filename, string extension, string fullPath, long size, bool isDirectory)
+        {
+            Filename = filename;
+            Extension = extension;
+            FullPath = fullPath;
+            Size = size;
+            IsDirectory = isDirectory;
+        }
+    }
+}
