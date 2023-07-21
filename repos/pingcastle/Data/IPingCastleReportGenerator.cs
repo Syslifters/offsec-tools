f5fbfb478128e597e218ed7207047cbec5934af1
cmahrl <christoph@syslifters.com>
2023-07-21 11:03:56 -0700

    vletoux/pingcastle@546c7fbb375e60215068d3bfa446d8de4251af6a

---
+++ repos/pingcastle/Data/IPingCastleReportGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Net;
+
+namespace PingCastle.Data
+{
+
+    public class PingCastleAnalyzerParameters
+    {
+        public string Server { get; set; }
+        public int Port { get; set; }
+        public NetworkCredential Credential { get; set; }
+        public bool PerformExtendedTrustDiscovery { get; set; }
+        public List<string> AdditionalNamesForDelegationAnalysis { get; set; }
+    }
+
+    public interface IPingCastleAnalyzer<T> where T : IPingCastleReport
+    {
+        T PerformAnalyze(PingCastleAnalyzerParameters parameters);
+    }
+}
