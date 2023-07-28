bd8afe6e72dd9e0280328d542e4fe4764e064e6d
cmahrl <christoph@syslifters.com>
2023-07-28 11:04:20 -0700

    BloodHoundAD/SharpHound@f4d613b596c5dc55dc963fcd6b8acaf86c15d89e

---
+++ repos/SharpHound/src/Client/Enums.cs
@@ -0,0 +1,28 @@
+﻿namespace Sharphound.Client
+{
+    /// <summary>
+    ///     Enums representing the possible collection methods specified in options
+    /// </summary>
+    public enum CollectionMethodOptions
+    {
+        None,
+        Group,
+        Session,
+        LoggedOn,
+        Trusts,
+        ACL,
+        ObjectProps,
+        RDP,
+        DCOM,
+        LocalAdmin,
+        PSRemote,
+        SPNTargets,
+        Container,
+        GPOLocalGroup,
+        LocalGroup,
+        Default,
+        DCOnly,
+        ComputerOnly,
+        All
+    }
+}
