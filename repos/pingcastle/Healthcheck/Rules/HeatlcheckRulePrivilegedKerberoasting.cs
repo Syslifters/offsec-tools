c4aba8e01583a517f38d9fee24c129773af30965
cmahrl <christoph@syslifters.com>
2023-09-22 11:03:56 -0700

    vletoux/pingcastle@51412bf7ad13c861d78b95707f762403364af3a8

---
+++ repos/pingcastle/Healthcheck/Rules/HeatlcheckRulePrivilegedKerberoasting.cs
@@ -0,0 +1,68 @@
+﻿using PingCastle.Graph.Reporting;
+//
+// Copyright (c) Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+using PingCastle.Rules;
+using System;
+using System.Collections.Generic;
+
+namespace PingCastle.Healthcheck.Rules
+{
+    [RuleModel("P-Kerberoasting", RiskRuleCategory.PrivilegedAccounts, RiskModelCategory.AccountTakeOver)]
+    [RuleComputation(RuleComputationType.PerDiscover, 5)]
+    [RuleIntroducedIn(2, 7)]
+    [RuleDurANSSI(1, "spn_priv", "Privileged accounts with SPN")]
+    [RuleMitreAttackTechnique(MitreAttackTechnique.StealorForgeKerberosTicketsKerberoasting)]
+    public class HeatlcheckRulePrivilegedKerberoasting : RuleBase<HealthcheckData>
+    {
+        protected override int? AnalyzeDataNew(HealthcheckData healthcheckData)
+        {
+            var dangerousGroups = new List<string>() {
+                GraphObjectReference.DomainAdministrators,
+                GraphObjectReference.EnterpriseAdministrators,
+                GraphObjectReference.SchemaAdministrators,
+                GraphObjectReference.Administrators,
+            };
+            foreach (var group in healthcheckData.PrivilegedGroups)
+            {
+                if (!dangerousGroups.Contains(group.GroupName))
+                {
+                    continue;
+                }
+                foreach (var user in group.Members)
+                {
+                    if (user == null)
+                        continue;
+                    if (user.IsService && user.PwdLastSet.AddDays(40) < DateTime.Now)
+                    {
+                        bool trap = false;
+                        if (healthcheckData.ListHoneyPot != null)
+                        {
+                            foreach (var account in healthcheckData.ListHoneyPot)
+                            {
+                                if (account == null)
+                                    continue;
+                                if (account.Name == user.Name || account.Name + "$" == user.Name)
+                                {
+                                    trap = true;
+                                    break;
+                                }
+                                if (account.DistinguishedName == user.DistinguishedName)
+                                {
+                                    trap = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if (!trap)
+                            AddRawDetail(group.GroupName, user.Name);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
