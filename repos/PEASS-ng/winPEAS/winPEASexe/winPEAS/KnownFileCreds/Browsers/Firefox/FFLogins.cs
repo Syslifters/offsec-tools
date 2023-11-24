fc5772f50b9bd63621ab2d8ba9d0c8e1e87d6749
cmahrl <christoph@syslifters.com>
2023-11-24 11:13:54 -0800

    carlospolop/PEASS-ng@31aed5cd927c57d80c91f03a54c15836ad98c506

---
+++ repos/PEASS-ng/winPEAS/winPEASexe/winPEAS/KnownFileCreds/Browsers/Firefox/FFLogins.cs
@@ -0,0 +1,10 @@
+﻿namespace winPEAS.KnownFileCreds.Browsers.Firefox
+{
+    class FFLogins
+    {
+        public long nextId { get; set; }
+        public LoginData[] logins { get; set; }
+        public string[] disabledHosts { get; set; }
+        public int version { get; set; }
+    }
+}
