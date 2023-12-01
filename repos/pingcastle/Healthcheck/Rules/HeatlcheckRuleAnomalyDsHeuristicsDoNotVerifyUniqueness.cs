21ad06435684645ff32bb76b223ce25b1ce2ad09
cmahrl <christoph@syslifters.com>
2023-12-01 11:04:03 -0800

    vletoux/pingcastle@51412bf7ad13c861d78b95707f762403364af3a8

---
+++ repos/pingcastle/Healthcheck/Rules/HeatlcheckRuleAnomalyDsHeuristicsDoNotVerifyUniqueness.cs
@@ -0,0 +1,27 @@
+﻿//
+// Copyright (c) Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+using PingCastle.Rules;
+
+namespace PingCastle.Healthcheck.Rules
+{
+    [RuleModel("A-DsHeuristicsDoNotVerifyUniqueness", RiskRuleCategory.Anomalies, RiskModelCategory.Reconnaissance)]
+    [RuleComputation(RuleComputationType.TriggerOnPresence, 5)]
+    [RuleDurANSSI(2, "dsheuristics_bad", "Dangerous dsHeuristics settings")]
+    [RuleIntroducedIn(2, 10, 1)]
+    [RuleMitreAttackTechnique(MitreAttackTechnique.ForcedAuthentication)]
+    public class HeatlcheckRuleAnomalyDsHeuristicsDoNotVerifyUniqueness : RuleBase<HealthcheckData>
+    {
+        protected override int? AnalyzeDataNew(HealthcheckData healthcheckData)
+        {
+            if (healthcheckData.DSHeuristics != null && healthcheckData.DSHeuristics.Length >= 21 && healthcheckData.DSHeuristics.Substring(20, 1) != "0")
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
