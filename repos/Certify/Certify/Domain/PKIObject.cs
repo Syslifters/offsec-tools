4420acde429078c3839aafd7c332748fc344f773
cmahrl <christoph@syslifters.com>
2025-03-14 12:27:20 -0700

    GhostPack/Certify@a2d230f607217b7b567d82ec7abb663311352131

---
+++ repos/Certify/Certify/Domain/PKIObject.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+using System.Security.Principal;
+
+namespace Certify.Domain
+{
+    class PKIObjectACE
+    {
+        public string? Type { get; }
+        public string? Rights { get; }
+        public Guid? ObjectType { get; }
+        public string? Principal { get; }
+
+        public PKIObjectACE(AccessControlType? type, ActiveDirectoryRights? rights, Guid? objectType, string? principal)
+        {
+            Type = type.ToString();
+            Rights = rights.ToString();
+            ObjectType = objectType;
+            Principal = principal;
+        }
+    }
+
+    class PKIObject : ADObject
+    {
+        public PKIObject(string? name, string? domainName, string distinguishedName, ActiveDirectorySecurity? securityDescriptor)
+            : base(distinguishedName, securityDescriptor)
+        {
+            Name = name;
+            DomainName = domainName;
+            DistinguishedName = distinguishedName;
+        }
+        public string? Name { get; }
+        public string? DomainName { get; }
+    }
+}
