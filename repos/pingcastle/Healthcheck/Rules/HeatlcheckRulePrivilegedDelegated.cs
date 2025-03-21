8b1f5f97830d872aff5840c45a01d366ad93a225
cmahrl <christoph@syslifters.com>
2025-03-21 12:08:37 -0700

    vletoux/pingcastle@7a6faa9a27ff6d3ea163dc4ef88c8b992b1adde0

---
+++ repos/pingcastle/Healthcheck/Rules/HeatlcheckRulePrivilegedDelegated.cs
@@ -0,0 +1,62 @@
+﻿//
+// Copyright (c) Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+using PingCastle.Rules;
+
+namespace PingCastle.Healthcheck.Rules
+{
+    [RuleModel("P-Delegated", RiskRuleCategory.PrivilegedAccounts, RiskModelCategory.AccountTakeOver)]
+    [RuleComputation(RuleComputationType.TriggerOnPresence, 20)]
+    [RuleSTIG("V-36435", "Delegation of privileged accounts must be prohibited.")]
+    [RuleMaturityLevel(3)]
+    [RuleMitreAttackMitigation(MitreAttackMitigation.ActiveDirectoryConfiguration)]
+    public class HeatlcheckRulePrivilegedDelegated : RuleBase<HealthcheckData>
+    {
+        protected override int? AnalyzeDataNew(HealthcheckData healthcheckData)
+        {
+            foreach (var member in healthcheckData.AllPrivilegedMembers)
+            {
+                if (member.CanBeDelegated)
+                {
+                    bool trap = false;
+                    if (healthcheckData.ListHoneyPot != null)
+                    {
+                        foreach (var account in healthcheckData.ListHoneyPot)
+                        {
+                            if (account == null)
+                                continue;
+                            if (account.Name == member.Name || account.Name + "$" == member.Name)
+                            {
+                                trap = true;
+                                break;
+                            }
+                            if (account.DistinguishedName == member.DistinguishedName)
+                            {
+                                trap = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (trap)
+                        continue;
+
+                    if (string.Equals(member.Class, "msDS-GroupManagedServiceAccount", System.StringComparison.OrdinalIgnoreCase))
+                        continue;
+
+                    if (healthcheckData.SchemaVersion < 69)
+                    {
+                        AddRawDetail(member.DistinguishedName, "Schema too low");
+                    }
+                    else if (!member.IsInProtectedUser)
+                    {
+                        AddRawDetail(member.DistinguishedName, "Not a protected user");
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ repos/pingcastle/Healthcheck/Rules/HeatlcheckRuleStaleDCRegistration.cs
@@ -0,0 +1,53 @@
+﻿//
+// Copyright (c) Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+using PingCastle.Rules;
+using System;
+
+namespace PingCastle.Healthcheck.Rules
+{
+    [RuleModel("S-DCRegistration", RiskRuleCategory.StaleObjects, RiskModelCategory.Provisioning)]
+    [RuleComputation(RuleComputationType.TriggerOnPresence, 10)]
+    [RuleIntroducedIn(2, 9)]
+    [RuleDurANSSI(1, "dc_inconsistent_uac", "Domain controllers in inconsistent state")]
+    [RuleMitreAttackTechnique(MitreAttackTechnique.RogueDomainController)]
+    public class HeatlcheckRuleStaleDCRegistrationEnabled : RuleBase<HealthcheckData>
+    {
+        protected override int? AnalyzeDataNew(HealthcheckData healthcheckData)
+        {
+            foreach (var dc in healthcheckData.DomainControllers)
+            {
+                if (!string.IsNullOrEmpty(dc.RegistrationProblem))
+                {
+                    bool isException = false;
+                    if (dc.AzureADKerberos)
+                    {
+                        continue;
+                    }
+                    if (InfrastructureSettings != null && InfrastructureSettings.Riverbeds != null)
+                    {
+                        foreach (var riverbed in InfrastructureSettings.Riverbeds)
+                        {
+                            var test = riverbed.samAccountName;
+                            if (test.EndsWith("$"))
+                                test = test.Substring(0, test.Length - 1);
+                            if (string.Equals(test, dc.DCName, StringComparison.OrdinalIgnoreCase))
+                            {
+                                isException = true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!isException)
+                    {
+                        AddRawDetail(dc.DCName, dc.RegistrationProblem);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
