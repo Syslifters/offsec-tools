9f62617a347089702144b2ec85206ce9171c3c24
cmahrl <christoph@syslifters.com>
2025-03-21 03:52:38 -0700

    vletoux/pingcastle@7a6faa9a27ff6d3ea163dc4ef88c8b992b1adde0

---
+++ repos/pingcastle/Graph/Database/Relation.cs
@@ -0,0 +1,24 @@
+﻿//
+// Copyright (c) Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace PingCastle.Graph.Database
+{
+    [DebuggerDisplay("From: {FromId} To: {ToId} Hints: {string.Join(\" \", Hint)}")]
+    public class Relation
+    {
+        public int FromId { get; set; }
+        public int ToId { get; set; }
+        public List<string> Hint { get; set; }
+
+        public Relation()
+        {
+            Hint = new List<string>();
+        }
+    }
+}
