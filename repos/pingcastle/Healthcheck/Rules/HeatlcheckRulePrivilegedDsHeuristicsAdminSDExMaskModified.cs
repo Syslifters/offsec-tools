ac31af7fefa159acd6fe009149caea75cd49b840
cmahrl <christoph@syslifters.com>
2025-03-29 04:12:13 -0700

    vletoux/pingcastle@7a6faa9a27ff6d3ea163dc4ef88c8b992b1adde0

---
+++ repos/pingcastle/Healthcheck/Rules/HeatlcheckRulePrivilegedDsHeuristicsAdminSDExMaskModified.cs
@@ -0,0 +1,27 @@
+﻿//
+// Copyright (c) Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+using PingCastle.Rules;
+
+namespace PingCastle.Healthcheck.Rules
+{
+    [RuleModel("P-DsHeuristicsAdminSDExMask", RiskRuleCategory.PrivilegedAccounts, RiskModelCategory.ACLCheck)]
+    [RuleComputation(RuleComputationType.TriggerOnPresence, 5)]
+    [RuleIntroducedIn(2, 7)]
+    [RuleDurANSSI(1, "dsheuristics_bad", "Dangerous dsHeuristics settings")]
+    [RuleMitreAttackMitigation(MitreAttackMitigation.PrivilegedAccountManagement)]
+    public class HeatlcheckRulePrivilegedDsHeuristicsAdminSDExMaskModified : RuleBase<HealthcheckData>
+    {
+        protected override int? AnalyzeDataNew(HealthcheckData healthcheckData)
+        {
+            if (healthcheckData.DSHeuristics != null && healthcheckData.DSHeuristics.Length >= 16 && healthcheckData.DSHeuristics.Substring(15, 1) != "0")
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ repos/pingcastle/Healthcheck/Rules/HeatlcheckRuleStaledOldNtlm.cs
@@ -0,0 +1,60 @@
+﻿//
+// Copyright (c) Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+using PingCastle.Rules;
+using System.Collections.Generic;
+
+namespace PingCastle.Healthcheck.Rules
+{
+    [RuleModel("S-OldNtlm", RiskRuleCategory.StaleObjects, RiskModelCategory.OldAuthenticationProtocols)]
+    [RuleComputation(RuleComputationType.TriggerOnPresence, 15)]
+    [RuleMitreAttackTechnique(MitreAttackTechnique.ManintheMiddleLLMNRNBTNSPoisoningandSMBRelay)]
+    [RuleANSSI("R37", "paragraph.3.6.2.1")]
+    [RuleMaturityLevel(2)]
+    [RuleIntroducedIn(2,11,2)]
+    public class HeatlcheckRuleStaledOldNtlm : RuleBase<HealthcheckData>
+    {
+        protected override int? AnalyzeDataNew(HealthcheckData healthcheckData)
+        {
+            var gpo = new Dictionary<IGPOReference, int>();
+            
+            if (healthcheckData.GPOLsaPolicy != null)
+            {
+                foreach (GPPSecurityPolicy policy in healthcheckData.GPOLsaPolicy)
+                {
+                    // The default level value for LmCompatibilityLevel for each version of Windows is as follows:
+                    // Windows XP: 0 Windows 2003: 2 Vista/2008 3 Win7/2008 R2 3
+                    // DC is not 5
+                    foreach (GPPSecurityPolicyProperty property in policy.Properties)
+                    {
+                        if (property.Property == "LmCompatibilityLevel")
+                        {
+                            gpo.Add(policy, property.Value);
+                        }
+                    }
+                }
+            }
+
+            var o = ApplyGPOPrority2(healthcheckData, gpo);
+           
+            bool found = false;
+            foreach (var v in o)
+            {
+                found = true;
+                if (v.Value < 5)
+                {
+                    AddRawDetail(v.Key.GPOName, v.Value);
+                }
+            }
+            
+            if (!found)
+            {
+                AddRawDetail("Windows default without an active GPO", "3");
+            }
+            return null;
+        }
+    }
+}
