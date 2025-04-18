a5260978cca814c2b2ddd8a7940a6f68f20a1120
cmahrl <christoph@syslifters.com>
2025-04-18 11:13:56 -0700

    GhostPack/Seatbelt@392171df84472591d4eae7ebd5b1cdc96ba91377

---
+++ repos/Seatbelt/Seatbelt/Commands/Products/SccmClientCommand.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Microsoft.Win32;
+
+namespace Seatbelt.Commands
+{
+    internal class SccmClientCommand : CommandBase
+    {
+        public override string Command => "SCCM";
+        public override string Description => "System Center Configuration Manager (SCCM) settings, if applicable";
+        public override CommandGroup[] Group => new[] { CommandGroup.System };
+        public override bool SupportRemote => true;
+        public Runtime ThisRunTime;
+
+        public SccmClientCommand(Runtime runtime) : base(runtime)
+        {
+            ThisRunTime = runtime;
+        }
+
+        public override IEnumerable<CommandDTOBase?> Execute(string[] args)
+        {
+            yield return new SccmClientDTO(
+                ThisRunTime.GetStringValue(RegistryHive.LocalMachine, @"SOFTWARE\Microsoft\CCMSetup", "LastValidMP"),
+                ThisRunTime.GetStringValue(RegistryHive.LocalMachine, @"SOFTWARE\Microsoft\SMS\Mobile Client", "AssignedSiteCode"),
+                ThisRunTime.GetStringValue(RegistryHive.LocalMachine, @"SOFTWARE\Microsoft\SMS\Mobile Client", "ProductVersion"),
+                ThisRunTime.GetStringValue(RegistryHive.LocalMachine, @"SOFTWARE\Microsoft\SMS\Mobile Client", "LastSuccessfulInstallParams")  // Sometimes contains the fallback server's hostname
+            );
+        }
+    }
+
+    internal class SccmClientDTO : CommandDTOBase
+    {
+        public SccmClientDTO(string? server, string? siteCode, string? productVersion, string? lastSuccessfulInstallParams)
+        {
+            Server = server;
+            SiteCode = siteCode;
+            ProductVersion = productVersion;
+            LastSuccessfulInstallParams = lastSuccessfulInstallParams;
+        }
+        public string? Server { get; }
+        public string? SiteCode { get; }
+        public string? ProductVersion { get; }
+        public string? LastSuccessfulInstallParams { get; }
+    }
+}
---
+++ repos/Seatbelt/Seatbelt/Output/Sinks/JsonFileOutputSink.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Web.Script.Serialization;
+using Seatbelt.Commands;
+
+namespace Seatbelt.Output.Sinks
+{
+    // Any sinks that output text to a location should inherit from this class
+    internal class JsonFileOutputSink : IOutputSink
+    {
+        private StreamWriter _stream;
+        private JavaScriptSerializer _json = new JavaScriptSerializer();
+
+        public JsonFileOutputSink(string file, bool filterResults)
+        {
+            if (File.Exists(file))
+            {
+                File.Delete(file);
+            }
+
+            _stream = File.CreateText(file);
+            _stream.AutoFlush = true;
+        }
+
+        public void WriteOutput(CommandDTOBase dto)
+        { 
+            //var obj = dtoCollection?.FirstOrDefault();
+            if (dto == null)
+            {
+                return;
+            }
+
+            // If the dto has a custom output sink, use it.  Otherwise, use the default output sink
+            var dtoType = dto.GetType();
+
+            var obj = new
+            {
+                Type = dtoType.ToString(),
+                Data = dto
+            };
+
+            string jsonStr;
+            try
+            {
+                jsonStr = _json.Serialize(obj);
+            }
+            catch(Exception e)
+            {
+                jsonStr = _json.Serialize(new
+                {
+                    Type = typeof(ErrorDTO).ToString(),
+                    Data = _json.Serialize(new ErrorDTO(e.ToString()))
+                });
+            }
+
+            _stream.WriteLine(jsonStr);
+        }
+
+        public void WriteVerbose(string message) => WriteOutput(new VerboseDTO(message));
+
+        public void WriteWarning(string message) => WriteOutput(new WarningDTO(message));
+
+        public void WriteError(string message) => WriteOutput(new ErrorDTO(message));
+
+        public void WriteHost(string message) => WriteOutput(new HostDTO(message));
+
+        public string GetOutput()
+        {
+            return "";
+        }
+
+        public void Dispose()
+        {
+            _stream.Dispose();
+        }
+    }
+}
