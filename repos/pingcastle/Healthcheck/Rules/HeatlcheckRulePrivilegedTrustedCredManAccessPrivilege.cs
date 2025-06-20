8bf87dbe4c2aa8b7c51f96ba45dda5af55d278b2
cmahrl <christoph@syslifters.com>
2025-06-20 11:06:55 -0700

    vletoux/pingcastle@548de82eb0d9631e7a244deffaaf63cfcd4fb460

---
+++ repos/pingcastle/Healthcheck/Rules/HeatlcheckRulePrivilegedTrustedCredManAccessPrivilege.cs
@@ -0,0 +1,36 @@
+﻿//
+// Copyright (c) Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+using PingCastle.Rules;
+using System;
+
+namespace PingCastle.Healthcheck.Rules
+{
+    [RuleModel("P-TrustedCredManAccessPrivilege", RiskRuleCategory.PrivilegedAccounts, RiskModelCategory.PrivilegeControl)]
+    [RuleComputation(RuleComputationType.TriggerOnPresence, 5)]
+    [RuleSTIG("V-63843", "The Access Credential Manager as a trusted caller user right must not be assigned to any groups or accounts", STIGFramework.Windows10)]
+    [RuleIntroducedIn(2, 8)]
+    [RuleMaturityLevel(3)]
+    [RuleMitreAttackMitigation(MitreAttackMitigation.PrivilegedAccountManagement)]
+    public class HeatlcheckRulePrivilegedTrustedCredManAccessPrivilege : RuleBase<HealthcheckData>
+    {
+        protected override int? AnalyzeDataNew(HealthcheckData healthcheckData)
+        {
+            foreach (var privilege in healthcheckData.GPPRightAssignment)
+            {
+                if (string.Equals(privilege.Privilege, "SeTrustedCredManAccessPrivilege", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (string.IsNullOrEmpty(privilege.User))
+                        continue;
+                    if (string.Equals(privilege.User, HealthcheckAnalyzer.EmptyUserPrivilege))
+                        continue;
+                    AddRawDetail(privilege.GPOName, privilege.User);
+                }
+            }
+            return null;
+        }
+    }
+}
