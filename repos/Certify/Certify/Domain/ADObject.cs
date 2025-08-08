2d8bffb3873ea304461575a60c6047b102f0c149
cmahrl <christoph@syslifters.com>
2025-08-08 01:52:17 -0700

    GhostPack/Certify@a2d230f607217b7b567d82ec7abb663311352131

---
+++ repos/Certify/Certify/Domain/ADObject.cs
@@ -0,0 +1,15 @@
+﻿using System.DirectoryServices;
+
+namespace Certify.Domain
+{
+    public class ADObject
+    {
+        public string DistinguishedName { get; set; }
+        public ActiveDirectorySecurity? SecurityDescriptor { get; set; }
+        public ADObject(string distinguishedName, ActiveDirectorySecurity? securityDescriptor)
+        {
+            DistinguishedName = distinguishedName;
+            SecurityDescriptor = securityDescriptor;    
+        }
+    }
+}
