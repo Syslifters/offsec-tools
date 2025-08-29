11ecd6b6335bb0a80c85ec11bf9cfe8faa546a07
cmahrl <christoph@syslifters.com>
2025-08-29 20:14:11 +0200

    vletoux/pingcastle@9c633fe984f25cf6fd7b10e77210280f856e84dd

---
+++ repos/pingcastle/Compatibility.cs
@@ -0,0 +1,51 @@
+﻿//
+// Copyright (c) Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+
+namespace System.Runtime.Serialization
+{
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Module, Inherited = false, AllowMultiple = true)]
+    internal sealed class ContractNamespaceAttribute : Attribute
+    {
+        private string clrNamespace;
+
+        private string contractNamespace;
+
+        public string ClrNamespace
+        {
+            get
+            {
+                return this.clrNamespace;
+            }
+            set
+            {
+                this.clrNamespace = value;
+            }
+        }
+
+        public string ContractNamespace
+        {
+            get
+            {
+                return this.contractNamespace;
+            }
+        }
+
+        public ContractNamespaceAttribute(string contractNamespace)
+        {
+            this.contractNamespace = contractNamespace;
+        }
+    }
+
+    // available in dotnet 3 but not on dotnet 2 which is needed for Windows 20001
+    [System.AttributeUsage(System.AttributeTargets.Field | System.AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    internal sealed class IgnoreDataMemberAttribute : Attribute
+    {
+        public IgnoreDataMemberAttribute()
+        {
+        }
+    }
+}
