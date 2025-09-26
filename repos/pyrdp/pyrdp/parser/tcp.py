2d28dbfc8f53b97fa85e55546e1e888d90dee56b
cmahrl <christoph@syslifters.com>
2025-09-26 20:06:22 +0200

    GoSecure/pyrdp@2147093a94a498f96642d0c951bb952d8462a914

---
+++ repos/pyrdp/pyrdp/parser/tcp.py
@@ -0,0 +1,17 @@
+#
+# This file is part of the PyRDP project.
+# Copyright (C) 2019, 2021 GoSecure Inc.
+# Licensed under the GPLv3 or later.
+#
+
+from pyrdp.parser import Parser
+from pyrdp.pdu import PDU
+from pyrdp.pdu.tcp import TCPPDU
+
+
+class TCPParser(Parser):
+    def doParse(self, data: bytes) -> TCPPDU:
+        return TCPPDU(data)
+
+    def write(self, pdu: PDU) -> bytes:
+        return pdu.payload
