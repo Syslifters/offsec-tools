ccab3ea01525541b02bdfe7d148eefb3e573e22d
cmahrl <christoph@syslifters.com>
2025-11-14 12:43:42 +0100

    GoSecure/pyrdp@2147093a94a498f96642d0c951bb952d8462a914

---
+++ repos/pyrdp/pyrdp/mcs/__init__.py
@@ -0,0 +1,7 @@
+#
+# This file is part of the PyRDP project.
+# Copyright (C) 2018 GoSecure Inc.
+# Licensed under the GPLv3 or later.
+#
+
+from pyrdp.mcs.channel import MCSChannel, MCSClientChannel, MCSServerChannel
