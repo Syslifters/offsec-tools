f375bc0effcb21223bd97b70a15681d3fd4d8fd1
cmahrl <christoph@syslifters.com>
2025-11-14 10:55:49 +0100

    vletoux/pingcastle@b43dcee39bb17a7c5443b6509a5601ea54adedd0

---
+++ repos/pingcastle/PingCastle/Healthcheck/HoneyPotSettings.cs
@@ -0,0 +1,111 @@
+﻿using System.Configuration;
+
+namespace PingCastle.Healthcheck
+{
+    internal class HoneyPotSettings : ConfigurationSection
+    {
+        static HoneyPotSettings cachedSettings = null;
+        public static HoneyPotSettings GetHoneyPotSettings()
+        {
+            if (cachedSettings == null)
+                cachedSettings = ConfigurationManager.GetSection("honeyPotSettings") as HoneyPotSettings;
+            return cachedSettings;
+        }
+
+        [ConfigurationProperty("HoneyPots", IsRequired = false)]
+        public HoneyPotsCollection HoneyPots
+        {
+            get
+            {
+                return base["HoneyPots"] as HoneyPotsCollection;
+            }
+        }
+    }
+
+    [ConfigurationCollection(typeof(SingleHoneyPotSettings), AddItemName = "HoneyPot")]
+    internal class HoneyPotsCollection : ConfigurationElementCollection
+    {
+        public HoneyPotsCollection()
+        {
+
+        }
+
+        public SingleHoneyPotSettings this[int index]
+        {
+            get { return (SingleHoneyPotSettings)BaseGet(index); }
+            set
+            {
+                if (BaseGet(index) != null)
+                {
+                    BaseRemoveAt(index);
+                }
+                BaseAdd(index, value);
+            }
+        }
+
+        public void Add(SingleHoneyPotSettings pluginConfig)
+        {
+            BaseAdd(pluginConfig);
+        }
+
+        public void Clear()
+        {
+            BaseClear();
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new SingleHoneyPotSettings();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((SingleHoneyPotSettings)element).samAccountName;
+        }
+
+        public void Remove(SingleHoneyPotSettings pluginConfig)
+        {
+            BaseRemove(pluginConfig.samAccountName);
+        }
+
+        public void RemoveAt(int index)
+        {
+            BaseRemoveAt(index);
+        }
+
+        public void Remove(string name)
+        {
+            BaseRemove(name);
+        }
+
+    }
+
+    public class SingleHoneyPotSettings : ConfigurationElement
+    {
+        [ConfigurationProperty("samAccountName")]
+        public string samAccountName
+        {
+            get
+            {
+                return base["samAccountName"] as string;
+            }
+            set
+            {
+                base["samAccountName"] = value;
+            }
+        }
+
+        [ConfigurationProperty("distinguishedName")]
+        public string distinguishedName
+        {
+            get
+            {
+                return base["distinguishedName"] as string;
+            }
+            set
+            {
+                base["distinguishedName"] = value;
+            }
+        }
+    }
+}
---
+++ repos/pingcastle/PingCastle/Scanners/Smb3QueryNetworkScanner.cs
@@ -0,0 +1,63 @@
+﻿//
+// Copyright (c) Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace PingCastle.Scanners
+{
+    public class Smb3QueryNetworkScanner : ScannerBase
+    {
+
+        public override string Name { get { return "smb3querynetwork"; } }
+        public override string Description { get { return "List all IP of the computer and the interface speed using SMB3. Authentication required. Used to find other networks such as the one used for administration."; } }
+
+        override protected string GetCsvHeader()
+        {
+            return "Computer\tInterfaceId\tIP\tSpeed (in bit/s)";
+        }
+
+        override protected string GetCsvData(string computer)
+        {
+            StringBuilder sb = new StringBuilder();
+            DisplayAdvancement(computer, "Connecting to SMB3");
+
+            var o = Smb2ProtocolTest.GetFCTL_QUERY_NETWORK_INFO(computer, Settings.Credential);
+            if (o == null)
+            {
+                sb.Append(computer);
+                sb.Append("\t");
+                sb.Append("Unable to retrive the information");
+            }
+            else
+            {
+                foreach (var info in o)
+                {
+                    if (sb.Length != 0)
+                        sb.Append("\r\n");
+                    sb.Append(computer);
+                    sb.Append("\t");
+                    sb.Append(info.Index);
+                    sb.Append("\t");
+                    sb.Append(info.IP);
+                    sb.Append("\t");
+                    sb.Append(info.LinkSpeed);
+                }
+            }
+            return sb.ToString();
+        }
+
+        private static void DisplayAdvancement(string computer, string data)
+        {
+            string value = "[" + DateTime.Now.ToLongTimeString() + "] " + data;
+            if (ScanningMode == 1)
+                UserInterfaceFactory.GetUserInterface().DisplayError(value);
+            Trace.WriteLine(value);
+        }
+    }
+
+}
