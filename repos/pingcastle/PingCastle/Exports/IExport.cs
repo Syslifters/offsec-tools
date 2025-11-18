a08064806e42a5e521ee5a44f9f0eb1b2baae823
cmahrl <christoph@syslifters.com>
2025-11-18 07:16:18 +0100

    vletoux/pingcastle@b43dcee39bb17a7c5443b6509a5601ea54adedd0

---
+++ repos/pingcastle/PingCastle/Exports/IExport.cs
@@ -0,0 +1,11 @@
+﻿namespace PingCastle.Exports
+{
+    public interface IExport
+    {
+        void Initialize(RuntimeSettings initialisationSettings);
+        void Export(string filename);
+        string Name { get; }
+        string Description { get; }
+        DisplayState QueryForAdditionalParameterInInteractiveMode();
+    }
+}
