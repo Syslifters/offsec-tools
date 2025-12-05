7d72f2a53b7458c4fbc6f4a117b9f23736ee1597
cmahrl <christoph@syslifters.com>
2025-12-05 20:22:59 +0100

    SnaffCon/Snaffler@05bfee0b6f90ff90a0b27152f80e0b56d6caf3e8

---
+++ repos/Snaffler/SnaffCore/Classifiers/EffectiveAccess.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace SnaffCore.Classifiers.EffectiveAccess
+{
+    public class RwStatus
+    {
+        public bool CanRead { get; set; }
+        public bool CanWrite { get; set; }
+        public bool CanModify { get; set; }
+    }
+
+}
