63f93139eb8329d0b54c3d7d3a039c33d18b0de6
cmahrl <christoph@syslifters.com>
2025-12-26 20:11:38 +0100

    vletoux/pingcastle@b43dcee39bb17a7c5443b6509a5601ea54adedd0

---
+++ repos/pingcastle/PingCastle/Cloud/Credentials/CredentialBase.cs
@@ -0,0 +1,99 @@
+﻿//
+// Copyright (c) Vincent LE TOUX for Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+using PingCastle.Cloud.RESTServices.Azure;
+using PingCastle.Cloud.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PingCastle.Cloud.Credentials
+{
+    public abstract class CredentialBase : IAzureCredential
+    {
+        public CredentialBase() : this(null)
+        {
+
+        }
+        public CredentialBase(string tenantid)
+        {
+            this.tenantId = tenantid;
+        }
+
+        Dictionary<Type, Token> cache = new Dictionary<Type, Token>();
+        public Token LastTokenQueried { get; protected set; }
+
+        public bool ForceRefreshByRefreshToken { get; set; }
+
+        public async Task<Token> GetToken<T>() where T : IAzureService
+        {
+            if (cache.ContainsKey(typeof(T)))
+            {
+              var  caschedToken = cache[typeof(T)];
+
+                var networkLatency = 5;
+                var expiresOn = DateTimeOffset.FromUnixTimeSeconds(caschedToken.expires_on).AddSeconds(-networkLatency);
+
+                if (expiresOn <= DateTime.UtcNow || ForceRefreshByRefreshToken)
+                {
+                    caschedToken = await TokenFactory.RefreshToken<T>(tenantId, caschedToken);
+                    UpdateTokenCache<T>(caschedToken);
+                }
+
+                return caschedToken;
+            }
+
+            var newToken = await TokenFactory.GetToken<T>(this);
+            UpdateTokenCache<T>(newToken);
+
+            return newToken;
+        }
+
+        private void UpdateTokenCache<T>(Token token) where T : IAzureService
+        {
+            if (token.expires_on == 0)
+            {
+                token.expires_on = (uint)((DateTimeOffset)DateTime.UtcNow.AddSeconds(token.expires_in)).ToUnixTimeSeconds();
+            }
+
+            LastTokenQueried = token;
+            cache[typeof(T)] = token;
+        }
+
+        string tenantId;
+        public string Tenantid
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(tenantId) && cache.Count > 0)
+                {
+                    var token = cache.Values.FirstOrDefault();
+                    tenantId = token.ToJwtToken().tid;
+                }
+                return tenantId;
+            }
+        }
+
+        string tenantidToQuery;
+        public string TenantidToQuery
+        {
+            get
+            {
+                return tenantidToQuery;
+            }
+            set
+            {
+                if (tenantidToQuery == value)
+                    return;
+                if (string.IsNullOrEmpty(value))
+                    return;
+                tenantidToQuery = value;
+                cache.Clear();
+            }
+        }
+    }
+}
---
+++ repos/pingcastle/PingCastle/Rules/IRuleScore.cs
@@ -0,0 +1,18 @@
+﻿//
+// Copyright (c) Ping Castle. All rights reserved.
+// https://www.pingcastle.com
+//
+// Licensed under the Non-Profit OSL. See LICENSE file in the project root for full license information.
+//
+
+namespace PingCastle.Rules
+{
+    public interface IRuleScore
+    {
+        RiskRuleCategory Category { get; }
+        RiskModelCategory Model { get; }
+        string RiskId { get; }
+        int Points { get; }
+        
+    }
+}
