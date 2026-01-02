998bb81c2d391c00f22c804ede88ce172fb30a00
cmahrl <christoph@syslifters.com>
2026-01-02 20:23:11 +0100

    GhostPack/Certify@d19679ead399f4d853e103541db40f39975617cf

---
+++ repos/Certify/Certify/Domain/CertificateAuthorityWebServices.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Certify.Domain
+{
+    public class CertificateAuthorityWebServices
+    {
+        public List<string> LegacyAspEnrollmentUrls { get; set; } = new List<string>();
+        public List<string> EnrollmentWebServiceUrls { get; set; } = new List<string>();
+        public List<string> EnrollmentPolicyWebServiceUrls { get; set; } = new List<string>();
+        public List<string> NetworkDeviceEnrollmentServiceUrls { get; set; } = new List<string>();
+    }
+}
