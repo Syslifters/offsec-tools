98fc847061982bf73c5846019930f223e7480aab
cmahrl <christoph@syslifters.com>
2026-01-23 20:13:59 +0100

    GoSecure/pyrdp@171f376534026f7699b89b7980434651ba68af5f

---
+++ repos/pyrdp/pyrdp/parser/__init__.py
@@ -0,0 +1,29 @@
+#
+# This file is part of the PyRDP project.
+# Copyright (C) 2018-2021 GoSecure Inc.
+# Licensed under the GPLv3 or later.
+#
+
+from pyrdp.parser.gcc import GCCParser
+from pyrdp.parser.mcs import MCSParser
+from pyrdp.parser.parser import Parser
+from pyrdp.parser.player import PlayerParser
+from pyrdp.parser.rdp.bitmap import BitmapParser
+from pyrdp.parser.rdp.client_info import ClientInfoParser
+from pyrdp.parser.rdp.connection import ClientConnectionParser, ServerConnectionParser
+from pyrdp.parser.rdp.fastpath import BasicFastPathParser, createFastPathParser, FastPathInputParser, \
+    FastPathOutputParser, FIPSFastPathParser, SignedFastPathParser
+from pyrdp.parser.rdp.orders import OrdersParser
+from pyrdp.parser.rdp.input import SlowPathInputParser
+from pyrdp.parser.rdp.licensing import LicensingParser
+from pyrdp.parser.rdp.negotiation import NegotiationRequestParser, NegotiationResponseParser
+from pyrdp.parser.rdp.ntlmssp import NTLMSSPParser
+from pyrdp.parser.rdp.pointer import PointerEventParser
+from pyrdp.parser.rdp.security import BasicSecurityParser, FIPSSecurityParser, SignedSecurityParser
+from pyrdp.parser.rdp.slowpath import SlowPathParser
+from pyrdp.parser.rdp.virtual_channel.clipboard import ClipboardParser
+from pyrdp.parser.rdp.virtual_channel.device_redirection import DeviceRedirectionParser
+from pyrdp.parser.rdp.virtual_channel.virtual_channel import VirtualChannelParser
+from pyrdp.parser.segmentation import SegmentationParser
+from pyrdp.parser.tpkt import TPKTParser
+from pyrdp.parser.x224 import X224Parser
