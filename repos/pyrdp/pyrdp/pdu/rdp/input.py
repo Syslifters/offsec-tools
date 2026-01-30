3d33b122f08a5d3a28b18305bc6bd4eac5feb670
cmahrl <christoph@syslifters.com>
2026-01-30 20:14:02 +0100

    GoSecure/pyrdp@171f376534026f7699b89b7980434651ba68af5f

---
+++ repos/pyrdp/pyrdp/pdu/rdp/input.py
@@ -0,0 +1,56 @@
+#
+# This file is part of the PyRDP project.
+# Copyright (C) 2018 GoSecure Inc.
+# Licensed under the GPLv3 or later.
+#
+
+from pyrdp.enum import InputEventType
+from pyrdp.pdu.pdu import PDU
+
+
+class SlowPathInput(PDU):
+    def __init__(self, eventTime, messageType):
+        super().__init__()
+        self.eventTime = eventTime
+        self.messageType = messageType
+
+
+class SynchronizeEvent(SlowPathInput):
+    def __init__(self, eventTime, flags):
+        SlowPathInput.__init__(self, eventTime, InputEventType.INPUT_EVENT_SYNC)
+        self.flags = flags
+
+
+class UnusedEvent(SlowPathInput):
+    def __init__(self, eventTime):
+        SlowPathInput.__init__(self, eventTime, InputEventType.INPUT_EVENT_UNUSED)
+
+
+class KeyboardEvent(SlowPathInput):
+    def __init__(self, eventTime, flags, keyCode):
+        SlowPathInput.__init__(self, eventTime, InputEventType.INPUT_EVENT_SCANCODE)
+        self.flags = flags
+        self.keyCode = keyCode
+
+
+class UnicodeKeyboardEvent(SlowPathInput):
+    def __init__(self, eventTime, flags, keyCode):
+        SlowPathInput.__init__(self, eventTime, InputEventType.INPUT_EVENT_UNICODE)
+        self.flags = flags
+        self.keyCode = keyCode
+
+
+class MouseEvent(SlowPathInput):
+    def __init__(self, eventTime, flags, x, y):
+        SlowPathInput.__init__(self, eventTime, InputEventType.INPUT_EVENT_MOUSE)
+        self.flags = flags
+        self.x = x
+        self.y = y
+
+
+class ExtendedMouseEvent(SlowPathInput):
+    def __init__(self, eventTime, flags, x, y):
+        SlowPathInput.__init__(self, eventTime, InputEventType.INPUT_EVENT_MOUSEX)
+        self.flags = flags
+        self.x = x
+        self.y = y
