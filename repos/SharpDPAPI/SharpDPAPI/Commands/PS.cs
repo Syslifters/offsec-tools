4b82925350d14acda8962418a371be8a78c3b99f
cmahrl <christoph@syslifters.com>
2026-02-13 20:19:14 +0100

    GhostPack/SharpDPAPI@4428263fe85bb1fde1640b6208d11dff6db4fb55

---
+++ repos/SharpDPAPI/SharpDPAPI/Commands/PS.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SharpDPAPI.Commands
+{
+    public class PS : ICommand
+    {
+        public static string CommandName => "ps";
+
+        public void Execute(Dictionary<string, string> arguments)
+        {
+            Console.WriteLine("\r\n[*] Action: Describe PSCredential .xml");
+
+            string target = "";
+            bool unprotect = false;         // whether to force CryptUnprotectData()
+            var server = "";
+
+            if (arguments.ContainsKey("/unprotect"))
+            {
+                Console.WriteLine("\r\n[*] Using CryptUnprotectData() for decryption.");
+                unprotect = true;
+            }
+            Console.WriteLine();
+
+            if (arguments.ContainsKey("/target"))
+            {
+                target = arguments["/target"];
+            }
+            else
+            {
+                Console.WriteLine("[X] A /target:<BASE64 | file.bin> must be supplied!");
+                return;
+            }
+
+            if (arguments.ContainsKey("/server"))
+            {
+                server = arguments["/server"];
+            }
+
+            // {GUID}:SHA1 keys are the only ones that don't start with /
+            Dictionary<string, string> masterkeys = new Dictionary<string, string>();
+            foreach (KeyValuePair<string, string> entry in arguments)
+            {
+                if (!entry.Key.StartsWith("/"))
+                {
+                    masterkeys.Add(entry.Key, entry.Value);
+                }
+            }
+            if (arguments.ContainsKey("/pvk"))
+            {
+                // use a domain DPAPI backup key to triage masterkeys
+                masterkeys = SharpDPAPI.Dpapi.PVKTriage(arguments);
+            }
+            else if (arguments.ContainsKey("/mkfile"))
+            {
+                masterkeys = SharpDPAPI.Helpers.ParseMasterKeyFile(arguments["/mkfile"]);
+            }
+            else if (arguments.ContainsKey("/password"))
+            {
+                Console.WriteLine("[*] Will decrypt user masterkeys with password: {0}\r\n", arguments["/password"]);
+                masterkeys = Triage.TriageUserMasterKeys(show: true, computerName: server, password: arguments["/password"]);
+            }
+            else if (arguments.ContainsKey("/ntlm"))
+            {
+                Console.WriteLine("[*] Will decrypt user masterkeys with NTLM hash: {0}\r\n", arguments["/ntlm"]);
+                masterkeys = Triage.TriageUserMasterKeys(show: true, computerName: server, ntlm: arguments["/ntlm"]);
+            }
+            else if (arguments.ContainsKey("/credkey"))
+            {
+                Console.WriteLine("[*] Will decrypt user masterkeys with credkey: {0}\r\n", arguments["/credkey"]);
+                masterkeys = Triage.TriageUserMasterKeys(show: true, computerName: server, credkey: arguments["/credkey"]);
+            }
+            else if (arguments.ContainsKey("/rpc"))
+            {
+                Console.WriteLine("[*] Will ask a domain controller to decrypt masterkeys for us\r\n");
+                masterkeys = Triage.TriageUserMasterKeys(show: true, rpc: true);
+            }
+
+            Triage.TriagePSCredFile(masterkeys, target, unprotect);
+        }
+    }
+}
