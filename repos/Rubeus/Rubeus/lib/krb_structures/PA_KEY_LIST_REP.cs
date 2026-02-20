c48121db9accc89f7c164b66965dd22e2fcf6173
cmahrl <christoph@syslifters.com>
2026-02-20 20:20:50 +0100

    GhostPack/Rubeus@74215f68ea70bd6a66c008da91bf5fe21d20b154

---
+++ repos/Rubeus/Rubeus/lib/krb_structures/PA_KEY_LIST_REP.cs
@@ -0,0 +1,29 @@
+﻿using Asn1;
+using System;
+using System.Text;
+
+namespace Rubeus
+{
+    public class PA_KEY_LIST_REP
+    {
+        // KERB-KEY-LIST-REP ::= SEQUENCE OF EncryptionKey
+        public PA_KEY_LIST_REP()
+        {
+            encryptionKey = new EncryptionKey();
+        }
+        public PA_KEY_LIST_REP(AsnElt body)
+        {
+            encryptionKey = new EncryptionKey(body);
+        }
+
+        public AsnElt Encode()
+        {
+            AsnElt encryptionKeyAsn = encryptionKey.Encode();
+            AsnElt encryptionKeySeq = AsnElt.Make(AsnElt.SEQUENCE, new[] { encryptionKeyAsn });
+            return encryptionKeySeq;
+        }
+
+        public EncryptionKey encryptionKey { get; set; }
+
+    }
+}
